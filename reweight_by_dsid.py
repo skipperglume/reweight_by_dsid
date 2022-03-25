5d2fe5cdf45c03f2715aadb3e0a668548f3d9642
GLUME <cdentimosh@gmail.com>
2022-03-25 01:25:33 +0100

    commit out of curiousity

--- reweight_by_dsid.py
+++ reweight_by_dsid.py
@@ -4,6 +4,7 @@
 from ROOT import *
 import argparse
 from array import array
+import os ,sys
 r_tag = ["MC16a","MC16d","MC16e"]
 
 dsid = ["364700", "364701", "364702", "364703", "364704",
@@ -14,9 +15,9 @@ dsid = ["364700", "364701", "364702", "364703", "364704",
 def parse_options():
         import argparse
 
-        parser = argparse.ArgumentParser(description='EventLooper')
-        parser.add_argument('-i','--input', help="The input file")
-        parser.add_argument('-o','--output', help="The output file")
+        parser = argparse.ArgumentParser(description='Reweight of multiple ntuples')
+        parser.add_argument('-d','--directory', help="The directory of ntuples to be reweighted")
+        parser.add_argument('-o','--output', help="The output directory")
         parser.add_argument('-t','--tree', default='IsolatedJet_tree', help="The ttree name")
         parser.add_argument('-H','--hist', default='MetaData_EventCount', help="Histogram containing cutflow")
         parser.add_argument('-w','--weight', default='sumw', help="Normalise by 'nevts' or 'sumw'")
@@ -26,40 +27,97 @@ def parse_options():
 
 def main():
         # Read the command-line options
-        opts =  parse_options()
-        
-        # Open input file and get ntuple
-        fin = TFile.Open(opts.input,"read")
-        t = fin.Get(opts.tree)
-        #t.Print()
-        print("Total entries: "+str(t.GetEntries()))
-        t.SetBranchStatus("*",1)
-        t.SetBranchStatus("jet_ConstitE",0)
-        h = fin.Get(opts.hist)
+        opts =  parse_options()        
+        onlyfiles = [f for f in os.listdir(opts.directory) if os.path.isfile(os.path.join(opts.directory, f))]
+        print(onlyfiles)
 
-        sumw = -1
-        if opts.weight=="nevts":
-            print("use nevts")
-            sumw = h.GetBinContent(1)
+        hadd_entries = [0,0]
+        part_entries = []
+        summed = [0,0]
+        
+        for ttree_name in onlyfiles:
+            fin = TFile.Open(opts.directory+'/'+ttree_name,"read")    
+            
+            t = fin.Get(opts.tree)
+            h = fin.Get(opts.hist)
+
+            sumw = -1
+
+            if opts.weight=="nevts":
+                sumw = h.GetBinContent(1)
+            if opts.weight=="sumw":
+                sumw = h.GetBinContent(3)
+            if ttree_name == 'a.root':
+                hadd_entries[0] = t.GetEntries()
+                hadd_entries[1] = sumw
+            else:
+                part_entries.append([])
+                part_entries[-1].append(t.GetEntries())
+                part_entries[-1].append(sumw)
+            fin.Close()
+        if os.path.exists(opts.output):
+            os.system("rm -rf "+opts.output)
+        os.system("mkdir "+opts.output)
+        
+        second = lambda x: x[1]
+        y = lambda : second (part_entries[0]) 
+        print(y)
+        #print(part_entries)
         if opts.weight=="sumw":
-            print("use sumw")
-            sumw = h.GetBinContent(3)
-        if opts.nevents==-1:
-            opts.nevents = t.GetEntries()
+            part_entries = [part_entries[i][1] for i in range(len(part_entries))]
+        if opts.weight=="nevts":
+            part_entries = [part_entries[i][0] for i in range(len(part_entries))]
         
 
+        #print(part_entries)
 
-        path_to_output = opts.output 
-        print(path_to_output)
+        DSID_TOTAL_WEIGHT = sum(part_entries)
+        #print("Total weight for DSID: ",DSID_TOTAL_WEIGHT)
         
-        print("Looping over",opts.nevents,"/",t.GetEntries(),"events in tree")
-        print("Sum of weights=",sumw)
-        if sumw==0:
-           print("ERROR: sumw==0")
+        
+        
+        for ttree_name in onlyfiles:
 
-        # Write ttree to new output file
-        print("Creating output file : ",opts.output)
-        fout = TFile(opts.output,"RECREATE")
+            
+            fin = TFile.Open(opts.directory+"/"+ttree_name,"read")
+            t = fin.Get(opts.tree)
+            #t.Print()
+            print("Total entries: "+str(t.GetEntries()))
+            t.SetBranchStatus("*",1)
+            t.SetBranchStatus("jet_ConstitE",0)
+            h = fin.Get(opts.hist)
+
+            sumw = DSID_TOTAL_WEIGHT
+               
+            
+            
+
+            path_to_output = opts.output 
+            print(path_to_output)
+            if(opts.nevents == -1):
+                opts.nevents = t.GetEntries()
+            print("Looping over: ",opts.nevents,"/",t.GetEntries()," events in tree")
+            print("Sum of weights=",sumw)
+            if sumw==0:
+                print("ERROR: sumw==0")
+
+            # Write ttree to new output file
+            if (opts.output[-1] == "/"):
+                opts.output = opts.output[:-1]
+            print(opts.output+"/"+"__reweighted__"+ttree_name)
+            
+            #print("Creating output file : ",opts.output)
+            #fout = TFile(opts.output,"RECREATE")
+
+
+            fin.Close()
+
+            '''
+
+
+
+        '''
+        
         tw = t.CloneTree(0)
         #tw.Print()
         tw.SetBranchStatus("*",1)
@@ -94,15 +152,15 @@ def main():
                 break
 
         tw.Write()
-        #for ievt in range(0,100):
-        #    tw.GetEntry(ievt)
-        #    print(ievt)
-        #    print("sumw: %f, weight: %f, weight_tot: %f"%(tw.sumw,tw.weight,tw.weight_tot))                                                    
+        
         fout.Close()
         fin.Close()
         print("OUTPUT "+path_to_output+ " is created")
-        '''
+        
         '''
 
 if __name__ == '__main__':
     main()
+
+
+#  python3 reweight_by_dsid.py  -d /eos/user/d/dtimoshy/IsolatedJets_Out/19022022/user.dtimoshy.364701.MC16a_364701_190222_tree.root -o /eos/user/d/dtimoshy/IsolatedJets_Out/19022022/__reweighted__MC16a_364701_190222_tree.root/
